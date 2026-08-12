9d036071b8338d017709bb6a19e5bf71ae2a4236
Aman Bhogal <aman.bhogal@innovixx.co.uk>
2026-08-12 16:24:34 +0000

    feat: add whereQuery and selectQuery to getTests endpoint

--- src/endpoints/test/getTests/index.ts
+++ src/endpoints/test/getTests/index.ts
@@ -3,6 +3,8 @@ import { maindb } from '../../../prisma/maindb/index.js';
 import { DB_RECORDS_DEFAULT_LIMIT, DB_RECORDS_MAX_LIMIT } from '../../../lib/constants.js';
 import { convertQuerySortToPrismaOrderBy } from '../../../utils/convertQuerySortToPrismaOrderBy/index.js';
 import { type EndpointHandler, type PaginatedDocs, type QueryParams } from '../../../lib/types.js';
+import { sanitizeWhitelistedWhere } from '../../../utils/sanitizeWhitelistedWhere/index.js';
+import { sanitizeWhitelistedSelect } from '../../../utils/sanitizeWhitelistedSelect/index.js';
 
 type Props = QueryParams
 
@@ -13,16 +15,19 @@ export const getTests: EndpointHandler<Props, PaginatedDocs<Test>> = async ({
 	sortBy,
 	where,
 }) => {
-	const total = await maindb.test.count({
-		...(where ? { where } : {}),
-	});
+	const whereQuery = sanitizeWhitelistedWhere(where, {});
+	const selectQuery = sanitizeWhitelistedSelect(select, {});
 
 	const items = await maindb.test.findMany({
-		...(where ? { where } : {}),
+		where: whereQuery,
 		skip: offset,
 		take: Math.min(limit || DB_RECORDS_DEFAULT_LIMIT, DB_RECORDS_MAX_LIMIT),
 		orderBy: convertQuerySortToPrismaOrderBy(sortBy),
-		...(select ? { select } : {}),
+		...(select ? { select: selectQuery } : {}),
+	});
+
+	const total = await maindb.test.count({
+		where: whereQuery,
 	});
 
 	return {
