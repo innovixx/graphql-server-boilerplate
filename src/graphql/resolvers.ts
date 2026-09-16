519f58ff082a97231741c163af53471b586bc0dd
Aman Bhogal <aman.bhogal@innovixx.co.uk>
2026-09-16 14:26:40 +0000

    feat: integrate base resolvers and type definitions from api-kit

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,7 +1,9 @@
 import merge from 'lodash.merge';
+import { baseResolvers } from '@innovixx/api-kit';
 import { testResolvers } from '../endpoints/test/index.js';
 
 export const resolvers = merge(
 	{},
+	baseResolvers,
 	testResolvers,
 );
--- src/graphql/typeDefs.ts
+++ src/graphql/typeDefs.ts
@@ -1,20 +1,12 @@
+import { baseTypeDefs } from '@innovixx/api-kit';
 import { fileURLToPath } from 'url';
 import path from 'path';
 import { loadFilesSync } from '@graphql-tools/load-files';
 import { mergeTypeDefs } from '@graphql-tools/merge';
-import fs from 'fs';
-import { print } from 'graphql';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const typesArray = loadFilesSync(path.join(__dirname, '../**/index.graphql'));
 
-const mergedTypeDefs = mergeTypeDefs(typesArray);
-const sdl = print(mergedTypeDefs);
-
-const outputPath = path.join(__dirname, 'schema.graphql');
-fs.mkdirSync(path.dirname(outputPath), { recursive: true });
-fs.writeFileSync(outputPath, sdl);
-
-export const typeDefs = mergedTypeDefs;
+export const typeDefs = mergeTypeDefs([baseTypeDefs, ...typesArray]);
